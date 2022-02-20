4af3bc4b6461fed447e73e8fde52755fbeae864d
akazwz <akazwz@pm.me>
2022-02-20 13:49:18 +0800

    add prisma

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,10 +1,35 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextFetchEvent } from 'next/server'
 
-export default function middleware (req: NextRequest) {
+export default async function middleware (req: NextRequest, evt: NextFetchEvent) {
   const { page, ip, geo, ua } = req
   const { name, params } = page
-  console.log('page name:' + name)
-  console.log('ip:' + ip)
-  console.log('city:' + geo?.city)
-  console.log('ua:' + ua)
+
+  /* geo info */
+  const country = geo?.country
+  const city = geo?.city
+  const latitude = geo?.latitude
+  const longitude = geo?.longitude
+  const region = geo?.region
+
+  /* ua info */
+  const isBot = ua?.isBot
+  const uaStr = ua?.ua
+  const browserName = ua?.browser.name
+  const browserVersion = ua?.browser.version
+  const os = ua?.os
+  const cpu = ua?.cpu
+
+  const device = ua?.device
+  const engine = ua?.engine
+
+  if (name === '/api/[shortid]') {
+    const jsonStr = JSON.stringify(params)
+    const { shortid } = JSON.parse(jsonStr)
+    /* get short id */
+    console.log(shortid)
+  }
+}
+
+const handleCollectShortIdInfo = (shortid: string,ip: string| undefined, geo:  ) => {
+
 }
