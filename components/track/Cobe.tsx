147c70b4a7d27c98f4536dbb59266f7951742746
akazwz <akazwz@pm.me>
2022-04-24 17:59:02 +0800

    update: cobe type

--- components/track/Cobe.tsx
+++ components/track/Cobe.tsx
@@ -1,53 +1,47 @@
 import React, { useEffect, useRef } from 'react'
-/* @ts-ignore */
-import createGlobe from 'cobe'
-import { LatLngTuple } from 'leaflet'
+import createGlobe, { Marker } from 'cobe'
 
-export interface ICobeprops {
-  size: number,
-  markers?: {
-    location: number[],
-    size: number
-  }[]
+export interface CobeIProps{
+	size:number,
+	markers:Marker[]
 }
 
-const Cobe = (props: ICobeprops) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  useEffect(() => {
-    let phi = 0
-    const globe = createGlobe(canvasRef.current, {
-      devicePixelRatio: 2,
-      width: props.size,
-      height: props.size,
-      phi: 0,
-      theta: 0,
-      dark: 1,
-      diffuse: 1.2,
-      mapSamples: 16000,
-      mapBrightness: 6,
-      baseColor: [0.3, 0.3, 0.3],
-      markerColor: [0.1, 0.8, 1],
-      glowColor: [1, 1, 1],
-      markers: props.markers || [{location: [39.9, 116.3], size: 0.05}],
-      onRender: (state: any) => {
-        // Called on every animation frame.
-        // `state` will be an empty object, return updated params.
-        state.phi = phi
-        phi += 0.007
-      },
-    })
+const Cobe = (props:CobeIProps) => {
+	const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	useEffect(() => {
+		if (!canvasRef.current) return
+		let phi = 0
+		const globe = createGlobe(canvasRef.current, {
+			devicePixelRatio: 2,
+			width: props.size,
+			height: props.size,
+			phi: 0,
+			theta: 0,
+			dark: 1,
+			diffuse: 1.2,
+			mapSamples: 16000,
+			mapBrightness: 6,
+			baseColor: [0.3, 0.3, 0.3],
+			markerColor: [0.1, 0.8, 1],
+			glowColor: [1, 1, 1],
+			markers: props.markers,
+			onRender: (state:any) => {
+				state.phi = phi
+				phi += 0.007
+			},
+		})
 
-    return () => {
-      globe.destroy()
-    }
-  }, [props.markers, props.size])
-  return (<canvas
-    ref={canvasRef}
-    style={{
-      width: props.size / 2,
-      height: props.size / 2,
-    }}
-  />)
+		return () => {
+			globe.destroy()
+		}
+	}, [props.markers, props.size])
+	return (<canvas
+		ref={canvasRef}
+		style={{
+			width: props.size / 2,
+			height: props.size / 2,
+		}}
+	/>)
 }
 
 export default Cobe
--- pages/track/dashboard.tsx
+++ pages/track/dashboard.tsx
@@ -2,209 +2,213 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 import {
-  Box,
-  Stack,
-  Center,
-  Spinner,
-  SimpleGrid,
+	Box,
+	Stack,
+	Center,
+	Spinner,
+	SimpleGrid,
 } from '@chakra-ui/react'
 import { VisitOverview } from '../../components/track/VisitOverview'
 import Cobe from '../../components/track/Cobe'
 import { LinkInfo, VisitInfo } from '../../src/types/track'
 import CommonPie from '../../components/track/CommonPie'
 import { generatePieDataFromStringArr, PieData } from '../../src/utils/chart'
+import { Marker } from 'cobe'
 
 const MyMap = dynamic(() => import('../../components/track/Map'), { ssr: false })
 
 export type ResTrackInfo = {
-  linkInfo: LinkInfo,
-  visitInfo: VisitInfo[]
+	linkInfo:LinkInfo,
+	visitInfo:VisitInfo[]
 }
 
 const mobileOSName = ['iOS', 'Android']
 
 const Dashboard = () => {
-  const router = useRouter()
+	const router = useRouter()
 
-  const [isShortIdExist, setIsShortIdExist] = useState<boolean>(false)
+	const [isShortIdExist, setIsShortIdExist] = useState<boolean>(false)
 
-  const [visitCount, setVisitCount] = useState<number>(0)
-  const [ipCount, setIPCount] = useState<number>(0)
-  const [mobileVisit, setMobileVisit] = useState<number>(0)
-  const [pcVisit, setPCVisit] = useState<number>(0)
+	const [visitCount, setVisitCount] = useState<number>(0)
+	const [ipCount, setIPCount] = useState<number>(0)
+	const [mobileVisit, setMobileVisit] = useState<number>(0)
+	const [pcVisit, setPCVisit] = useState<number>(0)
 
-  const [markerPoints, setMarkerPoints] = useState<[number, number][] | null>(null)
+	const [markerPoints, setMarkerPoints] = useState<Marker[]>([{ location: [39.9, 116.3], size: 0.05 }])
 
-  const [browserNamePieDataState, setBrowserNamePieData] = useState<PieData[]>([])
-  const [osNamePieDataState, setOSNamePieData] = useState<PieData[]>([])
-  const [deviceVendorDataState, setDeviceVendorPieData] = useState<PieData[]>([])
-  const [deviceModelPieDataState, setDeviceModelPieData] = useState<PieData[]>([])
+	const [locations, setLocations] = useState<[number, number][]>([])
 
-  useEffect(() => {
-    if (!router.isReady) return
-    const { shortid } = router.query
-    fetch(`/api/track/${shortid}`)
-      .then((res) => {
-        if (!res.ok) {
-          alert('short id error')
-          router.push('/').then()
-          return
-        }
-        res.json().then((resData: ResTrackInfo) => {
-          if (!resData.linkInfo) {
-            alert('no such short id')
-            router.push('/').then()
-            return
-          }
-          setIsShortIdExist(true)
-          setVisitCount(resData.visitInfo.length)
-          let ips: string[] = []
-          let browserNames: string[] = []
-          let osNames: string[] = []
-          let deviceVendors: string[] = []
-          let deviceModels: string[] = []
-          let mobileOSNames: string[] = []
-          let pcOSNames: string[] = []
-          let latlngs: [number, number][] = []
-          let map = new Map()
-          resData.visitInfo.map((visit) => {
-            const ip = visit.ip
-            /* ip count */
-            ips.push(ip)
+	const [browserNamePieDataState, setBrowserNamePieData] = useState<PieData[]>([])
+	const [osNamePieDataState, setOSNamePieData] = useState<PieData[]>([])
+	const [deviceVendorDataState, setDeviceVendorPieData] = useState<PieData[]>([])
+	const [deviceModelPieDataState, setDeviceModelPieData] = useState<PieData[]>([])
 
-            /* mobile or pc */
-            if (mobileOSName.includes(visit.ua.osName)) {
-              mobileOSNames.push(visit.ua.osName)
-            } else {
-              pcOSNames.push(visit.ua.osName)
-            }
+	useEffect(() => {
+		if (!router.isReady) return
+		const { shortid } = router.query
+		fetch(`/api/track/${shortid}`)
+			.then((res) => {
+				if (!res.ok) {
+					alert('short id error')
+					router.push('/').then()
+					return
+				}
+				res.json().then((resData:ResTrackInfo) => {
+					if (!resData.linkInfo) {
+						alert('no such short id')
+						router.push('/').then()
+						return
+					}
+					setIsShortIdExist(true)
+					setVisitCount(resData.visitInfo.length)
+					let ips:string[] = []
+					let browserNames:string[] = []
+					let osNames:string[] = []
+					let deviceVendors:string[] = []
+					let deviceModels:string[] = []
+					let mobileOSNames:string[] = []
+					let pcOSNames:string[] = []
+					let markersTemp:Marker[] = []
+					let locationsTemp:[number, number][] = []
+					// save location
+					resData.visitInfo.map((visit) => {
+						const ip = visit.ip
+						/* ip count */
+						ips.push(ip)
 
-            /* browser name */
-            const browserName = visit.ua.browserName
-            browserNames.push(browserName)
+						/* mobile or pc */
+						if (mobileOSName.includes(visit.ua.osName)) {
+							mobileOSNames.push(visit.ua.osName)
+						} else {
+							pcOSNames.push(visit.ua.osName)
+						}
 
-            /* os name */
-            const osName = visit.ua.osName
-            osNames.push(osName)
+						/* browser name */
+						const browserName = visit.ua.browserName
+						browserNames.push(browserName)
 
-            /* device vendor */
-            const deviceVendor = visit.ua.deviceVendor
-            deviceVendors.push(deviceVendor)
+						/* os name */
+						const osName = visit.ua.osName
+						osNames.push(osName)
 
-            /* device model */
-            const deviceModel = visit.ua.deviceModel
-            deviceModels.push(deviceModel)
+						/* device vendor */
+						const deviceVendor = visit.ua.deviceVendor
+						deviceVendors.push(deviceVendor)
 
-            /* lat lng */
-            if (map.get(Number(visit.geo.latitude)) !== Number(visit.geo.longitude)) {
-              latlngs.push([Number(visit.geo.latitude), Number(visit.geo.longitude)])
-            }
-            map.set(Number(visit.geo.latitude), Number(visit.geo.longitude))
-          })
+						/* device model */
+						const deviceModel = visit.ua.deviceModel
+						deviceModels.push(deviceModel)
 
-          setIPCount(Array.from(new Set(ips)).length)
-          setMobileVisit(mobileOSNames.length)
-          setPCVisit(pcOSNames.length)
-          setMarkerPoints(Array.from(new Set(latlngs)))
+						locationsTemp.push([Number(visit.geo.latitude), Number(visit.geo.longitude)])
+					})
 
-          /* browser name pie data */
-          setBrowserNamePieData(generatePieDataFromStringArr(browserNames))
-          /* os name pie data */
-          setOSNamePieData(generatePieDataFromStringArr(osNames))
-          /* device vendor */
-          setDeviceVendorPieData(generatePieDataFromStringArr(deviceVendors))
-          /* device model */
-          setDeviceModelPieData(generatePieDataFromStringArr(deviceModels))
-        })
+					setLocations([...Array.from(new Set(locationsTemp))])
 
-      }).catch((e) => {
-      console.log(e)
-      alert('error')
-      router.push('/').then()
-    })
-  }, [router])
+					setIPCount(Array.from(new Set(ips)).length)
+					setMobileVisit(mobileOSNames.length)
+					setPCVisit(pcOSNames.length)
 
-  if (!isShortIdExist) {
-    return (
-      <Center h={'100vh'}>
-        <Spinner/>
-      </Center>
-    )
-  }
+					locationsTemp.map((location) => {
+						markersTemp.push({ location, size: 0.05 })
+					})
 
-  return (
-    <Box w={'100vw'} minH={'100vh'} p={3} bg={'gray.900'}>
-      <VisitOverview
-        visitCount={visitCount}
-        ipCount={ipCount}
-        mobileVisit={mobileVisit}
-        pcVisit={pcVisit}
-      />
-      <Stack
-        mt={6}
-        mb={6}
-        ml={3}
-        mr={3}
-        direction={{ base: 'column', md: 'row' }}
-      >
-        <Box
-          w="100%"
-          rounded="lg"
-          boxShadow="dark-lg"
-          overflow="hidden"
-        >
-          <MyMap markersPoints={markerPoints}/>
-        </Box>
-        <Center>
-          <Cobe size={750} markers={markerPoints?.map((point) => {
-            return { location: point, size: 0.05 }
-          })}/>
-        </Center>
-      </Stack>
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 4 }}
-        gap={6}
-        m={3}
-      >
-        <Box
-          w={'100%'}
-          h={'27vh'}
-          bg={'gray.800'}
-          boxShadow={'dark-lg'}
-          rounded={'lg'}
-        >
-          <CommonPie title={'OS'} pieData={osNamePieDataState}/>
-        </Box>
-        <Box
-          w={'100%'}
-          h={'27vh'}
-          bg={'gray.800'}
-          boxShadow={'dark-lg'}
-          rounded={'lg'}
-        >
-          <CommonPie title={'Browser'} pieData={browserNamePieDataState}/>
-        </Box>
-        <Box
-          w={'100%'}
-          h={'27vh'}
-          bg={'gray.800'}
-          boxShadow={'dark-lg'}
-          rounded={'lg'}
-        >
-          <CommonPie title={'Device Vendor'} pieData={deviceVendorDataState}/>
-        </Box>
-        <Box
-          w={'100%'}
-          h={'27vh'}
-          bg={'gray.800'}
-          boxShadow={'dark-lg'}
-          rounded={'lg'}
-        >
-          <CommonPie title={'Device Model'} pieData={deviceModelPieDataState}/>
-        </Box>
-      </SimpleGrid>
-    </Box>
-  )
+					setMarkerPoints(markersTemp)
+					/* browser name pie data */
+					setBrowserNamePieData(generatePieDataFromStringArr(browserNames))
+					/* os name pie data */
+					setOSNamePieData(generatePieDataFromStringArr(osNames))
+					/* device vendor */
+					setDeviceVendorPieData(generatePieDataFromStringArr(deviceVendors))
+					/* device model */
+					setDeviceModelPieData(generatePieDataFromStringArr(deviceModels))
+				})
+
+			}).catch((e) => {
+			console.log(e)
+			alert('error')
+			router.push('/').then()
+		})
+	}, [router])
+
+	if (!isShortIdExist) {
+		return (
+			<Center h={'100vh'}>
+				<Spinner />
+			</Center>
+		)
+	}
+
+	return (
+		<Box w={'100vw'} minH={'100vh'} p={3} bg={'gray.900'}>
+			<VisitOverview
+				visitCount={visitCount}
+				ipCount={ipCount}
+				mobileVisit={mobileVisit}
+				pcVisit={pcVisit}
+			/>
+			<Stack
+				mt={6}
+				mb={6}
+				ml={3}
+				mr={3}
+				direction={{ base: 'column', md: 'row' }}
+			>
+				<Box
+					w="100%"
+					rounded="lg"
+					boxShadow="dark-lg"
+					overflow="hidden"
+				>
+					<MyMap markersPoints={locations} />
+				</Box>
+				<Center>
+					<Cobe size={750} markers={markerPoints} />
+				</Center>
+			</Stack>
+			<SimpleGrid
+				columns={{ base: 1, md: 2, lg: 4 }}
+				gap={6}
+				m={3}
+			>
+				<Box
+					w={'100%'}
+					h={'27vh'}
+					bg={'gray.800'}
+					boxShadow={'dark-lg'}
+					rounded={'lg'}
+				>
+					<CommonPie title={'OS'} pieData={osNamePieDataState} />
+				</Box>
+				<Box
+					w={'100%'}
+					h={'27vh'}
+					bg={'gray.800'}
+					boxShadow={'dark-lg'}
+					rounded={'lg'}
+				>
+					<CommonPie title={'Browser'} pieData={browserNamePieDataState} />
+				</Box>
+				<Box
+					w={'100%'}
+					h={'27vh'}
+					bg={'gray.800'}
+					boxShadow={'dark-lg'}
+					rounded={'lg'}
+				>
+					<CommonPie title={'Device Vendor'} pieData={deviceVendorDataState} />
+				</Box>
+				<Box
+					w={'100%'}
+					h={'27vh'}
+					bg={'gray.800'}
+					boxShadow={'dark-lg'}
+					rounded={'lg'}
+				>
+					<CommonPie title={'Device Model'} pieData={deviceModelPieDataState} />
+				</Box>
+			</SimpleGrid>
+		</Box>
+	)
 }
 
 export default Dashboard
