c28f0ff66f13a1e2d5ece28c5dd52fe3928a9eb9
akazwz <akazwz@pm.me>
2022-07-13 11:35:06 +0800

    update track api

--- pages/api/track/[short].ts
+++ pages/api/track/[short].ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { getUrl } from '../../../lib/redis'
+import prisma from '../../../lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	switch (req.method) {
@@ -10,5 +12,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 }
 
 const handleGetShortTrack = async (req: NextApiRequest, res: NextApiResponse) => {
+	const {short} = req.query
+	if (typeof short !== 'string') {
+		return res.status(400).json({ msg: 'params error' })
+	}
 
+	const url = await getUrl(short)
+
+	if (!url) {
+		return res.status(400).json({ msg: 'params error' })
+	}
+
+	const link = await prisma.link.findUnique({
+		where: {
+			shortCode: short,
+		}
+	})
+
+	const visits = await prisma.visit.findMany({
+		where: {
+			link: {
+				shortCode: short,
+			},
+		},
+	})
+
+	return res.status(200).json({
+		success: true,
+		short,
+		visits,
+	})
 }
